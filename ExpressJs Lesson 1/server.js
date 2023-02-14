5e063bbd952d2e09cba7ba35824964c202d5b483
felix_makinda <felixmakinda@gmail.com>
2023-02-14 13:46:46 +0300

    Final Commit

--- ExpressJs Lesson 1/server.js
+++ ExpressJs Lesson 1/server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(logger);
 
 app.get("/", (req, res) => {
   console.log("Hey! Things are working.");
-  res.render("index", { text: "Felix Makinda!" });
+  res.render("index", { text: "Felix Makinda Mogendi!" });
 });
 
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 app.listen(8081);
